bdfbd26589ac2fe669c18a0dff245df5af6f0f8e
magicdict <mynightelfplayer@hotmail.com>
2017-05-23 14:47:17 +0800

    Infrastructure 改为外部资源

--- InfraStructure/Bussiness.bat
+++
@@ -1,4 +0,0 @@
-E:
-cd E:\runmongo\bin
-mkdir E:\mongodb\CodeSnippet\DataBase
-mongod --port 28030 --storageEngine wiredTiger --dbpath E:\mongodb\CodeSnippet\DataBase --rest --nojournal >> E:\mongodb\CodeSnippet\DataBase\Logger.log
--- InfraStructure/FileStorage.bat
+++
@@ -1,4 +0,0 @@
-E:
-cd E:\runmongo\bin
-mkdir E:\mongodb\CodeSnippet\FileStorage
-mongod --port 28031 --storageEngine wiredTiger --dbpath E:\mongodb\CodeSnippet\FileStorage --rest --nojournal >> E:\mongodb\CodeSnippet\FileStorage\Logger.log
